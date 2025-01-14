4a8198759598c53478a7709723d609b41ff508a0
frndhoon <frndhoon@gmail.com>
2025-01-14 16:49:19 +0900

    🎉

---
+++ server.js
@@ -0,0 +1,46 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+
+// .env 파일 로드
+dotenv.config();
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    // 환경 변수에서 CLIENT_URL 가져오기
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+let connectedUsers = 0;
+
+io.on("connection", (socket) => {
+  console.log("사용자가 연결되었습니다");
+
+  const isLeftBoard = connectedUsers % 2 === 0;
+  socket.emit("assignBoard", isLeftBoard ? "left" : "right");
+  connectedUsers++;
+
+  socket.on("updateLeftBoard", (elements) => {
+    console.log("Left board update received");
+    io.emit("updateLeftBoard", elements);
+  });
+
+  socket.on("updateRightBoard", (elements) => {
+    console.log("Right board update received");
+    io.emit("updateRightBoard", elements);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("사용자가 연결을 해제했습니다");
+    connectedUsers--;
+  });
+});
+
+httpServer.listen(3001, () => {
+  console.log("서버가 3001 포트에서 실행 중입니다");
+});
